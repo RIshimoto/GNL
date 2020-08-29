e7e0e7f0e4ff8d11994e5c1ef68478152c8de912
rishimot <rishimot@student.42tokyo.jp>
2020-08-29 22:46:53 +0900

    ok

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,13 +6,13 @@
 /*   By: rishimot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/14 05:04:14 by rishimot          #+#    #+#             */
-/*   Updated: 2020/08/29 21:51:32 by rishimot         ###   ########.fr       */
+/*   Updated: 2020/08/29 22:39:28 by rishimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-void	super_free(char **p)
+void		super_free(char **p)
 {
 	free(*p);
 	*p = NULL;
@@ -20,54 +20,46 @@ void	super_free(char **p)
 
 static int	line_joint(char **p, char **save, char **line)
 {
-//	char	*ad_line;
 	char	*ad_p;
 	char	*nl;
 
-//	ad_line = *line;
 	ad_p = *p;
-	nl = ft_strchr(*p, '\n');
-	if (nl != NULL)
+	if ((nl = ft_strchr(*p, '\n')) != NULL)
 	{
 		if ((*line = ft_strjoin(*line, *p, nl - *p)) == NULL)
 			return (ERROR);
-//		super_free(&ad_line);
 		if (*(nl + 1) == '\0')
 			*save = NULL;
-		else 
-		{
-			if (!(*save = ft_strdup(nl + 1)))
-				return (ERROR);
-		}
+		else if (!(*save = ft_strdup(nl + 1)))
+			return (ERROR);
 		super_free(&ad_p);
 		return (SUCESS);
 	}
 	if ((*line = ft_strjoin(*line, *p, ft_strlen(*p))) == NULL)
 		return (ERROR);
-//	super_free(&ad_line);
 	return (CONTINUE);
 }
 
 int			get_next_line(int fd, char **line)
 {
 	char		*buf;
-	static char	*save;
+	static char	*save[1024];
 	ssize_t		read_size;
 	int			result;
 
 	*line = ft_strdup("");
 	if (*line == NULL || fd < 0 || fd == 1 || fd == 2 || BUFFER_SIZE < 1)
 		return (ERROR);
-	if (save != NULL)
-		if ((result = line_joint(&save, &save, line)) != CONTINUE)
-			return (result);
-	super_free(&save);
+	if (save[fd] != NULL &&\
+		(result = line_joint(&save[fd], &save[fd], line)) != CONTINUE)
+		return (result);
+	super_free(&save[fd]);
 	if (!(buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1)))
 		return (ERROR);
 	while ((read_size = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[read_size] = '\0';
-		if ((result = line_joint(&buf, &save, line)) != CONTINUE)
+		if ((result = line_joint(&buf, &save[fd], line)) != CONTINUE)
 			return (result);
 	}
 	super_free(&buf);
--- gnl_main.c
+++ gnl_main.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "get_next_line_bonus.h"
 //#include "get_next_line.h"
 
 int main(int argc, char **argv)
 {
-	int fd;
+	int fd[1024];
 	int i;
+	int j;
+	int r;
 	int l;
 	char *line;
 
 	if (argc > 1)
 		printf("%s\n", *argv);
+
+	j = 0;
+	fd[j++] = open("a.txt", O_RDONLY);
+	fd[j++] = open("b.txt", O_RDONLY);
+	//fd[j++] = open("c.txt", O_RDONLY);
+	//fd[j++] = open("d.txt", O_RDONLY);
 
 	//fd = open("bar.txt", O_RDONLY);
 	//fd = open("normal.txt", O_RDONLY);
@@ -21,16 +30,18 @@ int main(int argc, char **argv)
 	//fd = open("longtext.txt", O_RDONLY);
 	//fd = open("64bit_paragraph.txt", O_RDONLY);
 	//fd = open("64bit_line.txt", O_RDONLY);
-	fd = open("special.txt", O_RDONLY);
+	//fd = open("special.txt", O_RDONLY);
 	//fd = 10;
 
 	l = 0;
-	while (0 < (i = get_next_line(fd, &line)))
+	r = rand() % 1; 
+	while (0 < (i = get_next_line(fd[r], &line)))
 	{
 		printf("%d return flag : %d : '%s'\n", l, i, line);
 		l++;
 		free(line);
 		line = NULL;
+		r = rand() % 1; 
 	}
 	printf("%d return flag : %d : '%s'\n", l, i, line);
 	free(line);
